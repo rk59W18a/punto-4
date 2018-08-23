b5be8592f8bb7a6ff6fca11b01b0729aefa21efd
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-23 15:56:08 +0200

    Add files via upload

---
+++ RicercaPerCasaEditriceStrategy.java
@@ -0,0 +1,28 @@
+package dominio.parte2.punto4;
+
+import java.util.ArrayList;
+
+public class RicercaPerCasaEditriceStrategy implements IRicercaStrategy
+{
+    private String s;
+	
+	public RicercaPerCasaEditriceStrategy(String s)
+	{
+		this.s = s;
+	}
+	
+	public ArrayList <Risorsa> ricercaRisorsa(ArrayList <Risorsa> elencoris) 
+	{
+		 ArrayList <Risorsa> risorseCercate = new ArrayList <>();
+	   	    
+	   	 for(int i = 0; i < elencoris.size(); i++)
+	   	 {
+	   	     Risorsa ris = elencoris.get(i);
+	   	     
+	   	     if(((Libro)ris).getCasaEditrice().equalsIgnoreCase(s))
+	               risorseCercate.add(ris);
+	   	 }
+	   	    	                    
+	   	 return risorseCercate;
+	}
+}
